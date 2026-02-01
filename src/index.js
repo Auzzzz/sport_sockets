fcac363addacd9f0a00d3abf5dc4b404cd2a7e3a
Auzzzz <chris@downunderaussie.com>
2026-02-01 11:14:30 +1100

    init w/ basic express server

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Express server!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
