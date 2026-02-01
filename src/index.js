f9cf08dd2d169a1b3369067013c76d1ff4fbfee7
Auzzzz <chris@downunderaussie.com>
2026-02-01 16:45:49 +1100

    post/get matches

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
+
 
 const app = express();
 const PORT = 8000;
@@ -10,6 +12,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Express server!' });
 });
 
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
